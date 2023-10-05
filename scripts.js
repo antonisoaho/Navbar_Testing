9e600bf91589ddbddd9401828fbdbe1c76fd0300
antonisoaho <isoahoanton@gmail.com>
2023-10-05 10:22:23 +0200

    Styling a bit

--- scripts.js
+++ scripts.js
@@ -1,13 +1,26 @@
 function screenWidthChange(mediaQuery) {
   const checkbox = document.getElementById("showMenu");
+  const menuOpt = document.getElementsByClassName("menuOptions");
 
   if (mediaQuery.matches) {
+    for (let i = 0; i < menuOpt.length; i++) {
+      menuOpt[i].style.display = "none";
+    }
+
     checkbox.disabled = true;
     checkbox.checked = false;
+
+    for (let i = 0; i < menuOpt.length; i++) {
+      menuOpt[i].style.display = "flex";
+    }
     console.log("Window larger then 860px");
   } else {
     console.log("Window smaller then 860px");
     checkbox.disabled = false;
+    checkbox.checked = false;
+    for (let i = 0; i < menuOpt.length; i++) {
+      menuOpt[i].style.display = "inline-block";
+    }
   }
 }
 
